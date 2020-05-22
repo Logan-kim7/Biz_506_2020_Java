78ac647a9509e8020682a7a48e0c2c07c49f3db1
skynate22 <skynate22@nave.com>
2020-05-22 17:45:39 +0900

    for 명령문연습

--- Java_Hello/src/com/biz/control/For_09.java
+++ Java_Hello/src/com/biz/control/For_09.java
@@ -20,11 +20,10 @@ public class For_09 {
 			for(int j = 0 ; j < 5 ; j++) {
 				System.out.print("* ");
 			}
-
-		} System.out.println("===================");
-		
-		
-		for(int i= 0 ; i < 5 ; i++) {
+			System.out.println();
+		} 
+		System.out.println("===================");
+			for(int i= 0 ; i < 5 ; i++) {
 			for(int j = 0 ; j < i; j++) {
 				System.out.print("* ");
 			}
